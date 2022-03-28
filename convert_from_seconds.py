dfaa0c070bda55c0fee14e5b4c59871a1273a97a
mellowc0de <adamalcala1988@gmail.com>
2022-03-28 16:52:00 -0700

    misspelled seconds
    corrected misspelling of seconds in the def function line
    def convert_seeconds(seconds):  >> def convert_seconds(seconds):

--- convert_from_seconds.py
+++ convert_from_seconds.py
@@ -1,4 +1,4 @@
-def convert_seeconds(seconds):
+def convert_seconds(seconds):
   hours = seconds // 3600
   minutes = (seconds - hours * 3600) // 60
   remaining_seconds = seconds - hours * 3600 - minutes * 60
