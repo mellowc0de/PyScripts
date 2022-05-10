ae25afd55782b27ff9364d194af958fba005e1da
mellowc0de <adamalcala1988@gmail.com>
2022-05-10 20:41:58 +0000

    added alternative print output in comments

--- string_formats.py
+++ string_formats.py
@@ -2,3 +2,6 @@
 name = input('Please enter your first name: ')
 number = len(name) * 3
 print("Hello {}, your number is {}".format(name, number))
+
+# Alternatively, 
+# print("YOur lucky number is {number}, {name}."format(name=name, number=len(name)*3))
