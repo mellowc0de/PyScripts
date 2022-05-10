90f7489f5a19aedfa0c71b66ac9492784491c3b8
mellowc0de <adamalcala1988@gmail.com>
2022-05-10 21:00:26 +0000

    added input and print to function

--- student_grade.py
+++ student_grade.py
@@ -1,5 +1,5 @@
 name = input('Enter student name: ')
-grade = input('Enter student\'s grade: ')
+grade = int(input('Enter student\'s grade: '))
 
 def student_grade(name, grade):
 	return "{name} received {grade}% on the exam".format(name=name, grade=grade)
@@ -7,3 +7,5 @@ def student_grade(name, grade):
 #print(student_grade("Reed", 80))
 #print(student_grade("Paige", 92))
 #print(student_grade("Jesse", 85))
+
+print(student_grade(name, grade))
