ed491c6d8ca4ac6645571541869046324575bf32
mellowc0de <adamalcala1988@gmail.com>
2022-05-10 20:52:00 +0000

    Created student_grade.py file and modified script to take user input for student name and grade.

---
+++ student_grade.py
@@ -0,0 +1,9 @@
+name = input('Enter student name: ')
+grade = input('Enter student\'s grade: ')
+
+def student_grade(name, grade):
+	return "{name} received {grade}% on the exam".format(name=name, grade=grade)
+
+#print(student_grade("Reed", 80))
+#print(student_grade("Paige", 92))
+#print(student_grade("Jesse", 85))
