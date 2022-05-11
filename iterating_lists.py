2b444ce5072eac7a4da3fbe9b3c7394cd32bfeb4
mellowc0de <adamalcala1988@gmail.com>
2022-05-11 02:11:03 +0000

    created iterating_lists.py file

--- iterating_lists.py
+++ iterating_lists.py
@@ -4,3 +4,11 @@ for animal in animals:
   chars += len(animal)
 
 print("Total characters: {} | Average length: {}".format(chars, chars/len(animals)))
+for index, animal in enumerate(animals):
+  print("{} - {}".format(index + 1, animal))
+
+print("----------------------------------------------")
+# Enumerate a list
+winners = ["Ashley", "Dylan", "Reese"]
+for index, person in enumerate(winners):
+  print("{} - {}".format(index + 1, person))
