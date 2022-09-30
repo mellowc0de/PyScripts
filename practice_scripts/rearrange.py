ecf2cf9ae33ec36f154fc8344bb7507efc01af26
adamalcala <adamalcala1988@gmail.com>
2022-09-30 03:41:45 -0700

    fixed TypeError: 'NoneType' object is not subscriptable

--- practice_scripts/rearrange.py
+++ practice_scripts/rearrange.py
@@ -5,4 +5,6 @@ import re
 def rearrange_name(name):
     """ A function that take in a first name and a last name and displays them in reverse order """
     result = re.search(r"^([\w .]*), ([\w .]*)$", name)
+    if result is None:
+        return ""
     return "{} {}".format(result[2], result[1])
