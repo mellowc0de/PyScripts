58052ffe77ca1fb34c679db7bc0392de5f70b79a
adamalcala <adamalcala1988@gmail.com>
2022-09-30 03:38:47 -0700

    added test_empty function

--- practice_scripts/rearrange_test.py
+++ practice_scripts/rearrange_test.py
@@ -6,5 +6,10 @@ class TestRearrange(unittest.TestCase):
         testcase = "Lovelace, Ada"
         expected = "Ada Lovelace"
         self.assertEqual(rearrange_name(testcase), expected)
+    
+    def test_empty(self):
+        testcase = ""
+        expected = ""
+        self.assertEqual(rearrange_name(testcase), expected)
 
 unittest.main()
