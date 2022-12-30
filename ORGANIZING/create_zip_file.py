ad55ce2f8495d747e85ba63a2ca41fc86420cfb4
adamalcala <adamalcala1988@gmail.com>
2022-12-30 08:20:00 -0800

    modified create_zip.py

--- ORGANIZING/create_zip_file.py
+++ ORGANIZING/create_zip_file.py
@@ -1,5 +1,17 @@
 import zipfile
+import os
+from pathlib import Path
 
-newZip = zipfile.ZipFile('resumes.zip', 'w')
-newZip.write('test.txt', compress_type=zipfile.ZIP_DEFLATED)
+c = Path.cwd()
+link = c / 'Python_Scripts' / 'ORGANIZING'
+
+file = 'resumes.zip'
+text_file = 'test.txt'
+
+zip_file = link / file
+
+write_file = link / text_file
+
+newZip = zipfile.ZipFile(zip_file, 'w')
+newZip.write(write_file, compress_type=zipfile.ZIP_DEFLATED)
 newZip.close()
